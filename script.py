86784eec0bdbff21ac6d604745b8f152fdc2d830
MrSabin <sabin.21011986@gmail.com>
2022-08-26 18:33:20 +0300

    Deleted unneeded variable

--- script.py
+++ script.py
@@ -1,18 +1,30 @@
 import random
 
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Subject, Commendation
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 def fix_marks(schoolkid):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
-    points = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
-    for record in points:
-        record.points = 5
-        record.save()
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid)
+    except ObjectDoesNotExist:
+        print("Ученик не найден")
+        return
+    except MultipleObjectsReturned:
+        print("Проверьте правильность введенных ФИО ученика")
+        return
+    Mark.objects.filter(schoolkid=child, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid)
+    except ObjectDoesNotExist:
+        print("Ученик не найден")
+        return
+    except MultipleObjectsReturned:
+        print("Проверьте правильность введенных ФИО ученика")
+        return
     all_chastisements = Chastisement.objects.filter(schoolkid=child)
     all_chastisements.delete()
 
@@ -23,10 +35,21 @@ def create_commendation(schoolkid, lesson):
     with open("commendations.txt", "r") as file:
         commendations = file.readlines()
         commendation = random.choice(commendations)
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
-    lesson = Lesson.objects.filter(year_of_study=child.year_of_study,
-                                   group_letter=child.group_letter,
-                                   subject__title=lesson).order_by("date").last()
+    try:
+        child = Schoolkid.objects.get(full_name__contains=schoolkid)
+    except ObjectDoesNotExist:
+        print("Ученик не найден")
+        return
+    except MultipleObjectsReturned:
+        print("Проверьте правильность введенных ФИО ученика")
+        return
+    try:
+        lesson = Lesson.objects.filter(year_of_study=child.year_of_study,
+                                       group_letter=child.group_letter,
+                                       subject__title=lesson).order_by("date").last()
+    except ObjectDoesNotExist:
+        print("Предмет не найден")
+        return
     Commendation.objects.create(text=commendation,
                                 created=lesson.date,
                                 schoolkid=child,
@@ -34,7 +57,7 @@ def create_commendation(schoolkid, lesson):
                                 teacher=lesson.teacher)
 
 
-name = "Пахомов Викентий"
+name = "Фролов Иван"
 subject = "Математика"
 fix_marks(name)
 remove_chastisements(name)
